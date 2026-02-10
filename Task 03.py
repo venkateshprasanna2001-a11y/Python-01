8e5b0c7ecd863cf8a34018ca8a3aabea349fd952
venkateshprasanna2001-a11y <venkateshprasanna2001@gmail.com>
2026-02-10 20:31:43 +0530

    Create Task 03.py

---
+++ Task 03.py
@@ -0,0 +1,9 @@
+viscosity = float(input("Enter Viscoity value: "))
+
+if viscosity  >= 80 and viscosity <= 120 :
+    print("viscosity OK")
+else:          
+     print("viscosity Out of Range")     
+     print("viscosity Out of Range")  
+
+   
